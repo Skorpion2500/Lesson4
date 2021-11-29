79e92eac3403a968e10a1e55b747f3c7ef202430
Skorpion2500 <94473965+Skorpion2500@users.noreply.github.com>
2021-11-29 20:46:20 +0300

    Update Занятие 4.py

--- Занятие 4.py
+++ Занятие 4.py
@@ -2,7 +2,7 @@ import random
 
 ############## Сортировка методом пузырька
 l=list(input('Введите список:'))
-for i in range(0,len(l)-1):
+for i in range(len(l)-1):
     for j in range(len(l)-1):
         if (l[j]>l[j+1]):
             c=l[j]
